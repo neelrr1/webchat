835f398277f9a1eabe7b6114b97609d70341efe9
Neel Raja <neelrr1@gmail.com>
2023-12-16 14:30:59 -0800

    Refactor middleware and add CORS headers

--- src/main.go
+++ src/main.go
@@ -27,8 +27,30 @@ type Chatter struct {
 	color string
 }
 
+// "Middleware" functions
 func logRequest(r *http.Request) {
 	log.Printf("INFO: %s %s - %s", r.Method, r.URL.Path, r.RemoteAddr)
+}
+
+func setCorsHeaders(w http.ResponseWriter, r *http.Request) {
+	origin := r.Header.Get("Origin")
+	headers := w.Header()
+	headers.Add("Access-Control-Allow-Origin", origin)
+	headers.Add("Access-Control-Allow-Headers", "Accept, Accept-Language, Content-Language, Content-Type, hx-target, hx-current-url, hx-trigger, hx-request")
+	headers.Add("Access-Control-Allow-Methods", "*")
+	w.WriteHeader(http.StatusOK)
+}
+
+// Wrapper function to execute custom "middleware"
+func handleRoute(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+		logRequest(r)
+
+		setCorsHeaders(w, r)
+		if r.Method != "OPTIONS" {
+			handler(w, r)
+		}
+	})
 }
 
 // TODO: Explore not using a polling architecture for messages
@@ -36,15 +58,16 @@ func logRequest(r *http.Request) {
 func main() {
 	http.HandleFunc("/static/", handleStatic)
 	http.HandleFunc("/", handleIndex)
-	http.HandleFunc("/send", handleSend)
-	http.HandleFunc("/messages", handleMessages)
+
+	// These handlers need middleware
+	handleRoute("/send", handleSend)
+	handleRoute("/messages", handleMessages)
 
 	fmt.Printf("Listening on port %s...\n", PORT)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", PORT), nil))
 }
 
 func handleMessages(w http.ResponseWriter, r *http.Request) {
-	logRequest(r)
 	messagesLock.RLock()
 	defer messagesLock.RUnlock()
 
@@ -52,7 +75,6 @@ func handleMessages(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleSend(w http.ResponseWriter, r *http.Request) {
-	logRequest(r)
 	time := time.Now()
 
 	bytes, err := ioutil.ReadAll(r.Body)
